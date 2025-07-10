b18cab4681f5c603c45fb27817703a859f3b7bcd
AlexeyShchukin <shchukin.developer@gmail.com>
2025-07-10 12:04:48 +0200

    fix: Fixed field deleted_at in BaseFieldsModel

--- src/projects/models/base_model.py
+++ src/projects/models/base_model.py
@@ -6,7 +6,7 @@ from django.db import models
 class BaseFieldsModel(models.Model):
     created_at: datetime = models.DateField(auto_now_add=True)
     updated_at: datetime = models.DateField(auto_now=True)
-    deleted_at: datetime = models.DateField(auto_now=True)
+    deleted_at: datetime = models.DateField(null=True, blank=True)
     deleted: bool = models.BooleanField(default=False, db_default=False)
 
     class Meta:
