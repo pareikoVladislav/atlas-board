0131d569efc3a6cb2d8512749c53a7359fe39438
beper223 <zashalovskiy@gmail.com>
2025-07-11 09:11:22 +0200

    add admin project

---
+++ src/projects/admin/project_admin.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+
+from src.projects.models.project import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    # Отображаемые поля в списке
+    list_display = (
+        'name',
+        'status',
+        'start_date',
+        'owner',
+        'description',
+        'created_at',
+        'end_date'
+    )
+    # Поля фильтрации
+    list_filter = (
+        'status',
+        'start_date',
+        'owner',
+        'created_at',
+        'end_date',
+    )
+    # Поля поиска
+    search_fields = (
+        'name',
+        'description',
+        'owner__first_name',
+        'owner__last_name',
+    )
+    # Сортировка: новые проекты сверху
+    ordering = ('-created_at',)
+    # Количество объектов на странице
+    list_per_page = 20
+    # Быстрое редактирование
+    list_editable = ('status', 'owner')
+    # Группировка полей в форме добавления/редактирования
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('name', 'description', 'owner')
+        }),
+        ('Status & Timeline', {
+            'fields': ('status', 'start_date', 'end_date')
+        }),
+        # ('Files', {
+        #     'fields': ('files',),
+        # }),
+        ('System Fields', {
+            'fields': ('created_at', 'updated_at', 'deleted_at'),
+            'classes': ('collapse',),
+        }),
+    )
+
+    # Только для чтения
+    readonly_fields = ('created_at', 'updated_at', 'deleted_at')
