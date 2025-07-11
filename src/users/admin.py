0f9d78b4a383a5db63c6fb2764b966c851612f59
AlexeyShchukin <shchukin.developer@gmail.com>
2025-07-11 09:14:00 +0200

    feat: Create user-admin panel

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from src.users.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(BaseUserAdmin):
+    model = User
+
+    list_display = [
+        'email',
+        'first_name',
+        'last_name',
+        'position',
+        'is_active',
+        'is_staff',
+        'date_joined'
+    ]
+    list_filter = [
+        'is_active',
+        'is_staff',
+        'is_superuser',
+        'position',
+        'date_joined'
+    ]
+    search_fields = [
+        'email',
+        'first_name',
+        'last_name'
+    ]
+    ordering = ['email']
+
+    fieldsets = (
+        (
+            None, {
+            "fields": ('password', 'email')
+        }
+         ),
+        (
+            _("Personal info"), {
+            "fields": ('first_name', 'last_name', 'position')
+        }
+         ),
+        (
+            _("Permissions"), {
+            "fields": ('is_superuser', 'groups', 'user_permissions', 'is_active', 'is_staff')
+        }
+         ),
+        (
+            _("Important dates"), {
+                "fields": ('last_login',)
+            }
+        )
+    )
+
+    add_fieldsets = (
+        (
+            None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'position')
+            }
+         ),
+    )
+
+
+admin.site.unregister(Group)
+admin.site.register(Group)
