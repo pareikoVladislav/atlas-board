12fadf5825bb320e64264b4f29283f149af27b2f
anaseller <seller.anst@gmail.com>
2025-07-16 13:50:22 +0200

    change task15 w new commit from task13

--- src/projects/repositories/project_repository.py
+++ src/projects/repositories/project_repository.py
@@ -1,13 +1,7 @@
 from src.projects.models.project import Project
-# from src.projects.repositories.base import BaseRepository  # пока не реализован
-
-class BaseRepository:
-    pass
+from src.projects.repositories.base import BaseRepository
 
 
 class ProjectRepository(BaseRepository):
     def __init__(self):
-        self.model = Project
-
-    def get_all(self):
-        return self.model.objects.all()
+        super().__init__(Project)
